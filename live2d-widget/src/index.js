6fb2fd25a3cb6115a6c4395db5a201bb3ee4f6ad
ccMaruy <yujin1261853341@gmail.com>
2024-08-18 16:52:50 +0200

    Site updated: 2024-08-18 16:52:48

--- live2d-widget/src/index.js
+++ live2d-widget/src/index.js
@@ -43,21 +43,21 @@ function loadWidget(config) {
                 }
             }
         }
-        const text = `欢迎阅读<span>「${document.title.split(" - ")[0]}」</span>`;
+        const text = `Bienvenidos a <span>「${document.title.split(" - ")[0]}」</span>`;
         let from;
         if (document.referrer !== "") {
             const referrer = new URL(document.referrer),
                 domain = referrer.hostname.split(".")[1];
             const domains = {
-                "baidu": "百度",
-                "so": "360搜索",
-                "google": "谷歌搜索"
+                "baidu": "Baidu",
+                "so": "360",
+                "google": "Google"
             };
             if (location.hostname === referrer.hostname) return text;
 
             if (domain in domains) from = domains[domain];
             else from = referrer.hostname;
-            return `Hello！来自 <span>${from}</span> 的朋友<br>${text}`;
+            return `Hello！Amigo de <span>${from}</span><br>${text}`;
         }
         return text;
     }
@@ -149,7 +149,7 @@ function initWidget(config, apiPath) {
         };
     }
     document.body.insertAdjacentHTML("beforeend", `<div id="waifu-toggle">
-            <span>看板娘</span>
+            <span>Poster Girl</span>
         </div>`);
     const toggle = document.getElementById("waifu-toggle");
     toggle.addEventListener("click", () => {
